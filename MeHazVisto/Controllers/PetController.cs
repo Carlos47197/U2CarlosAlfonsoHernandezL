9f0fd77bd735fa577e3125b7cd00839317d6665e
Carlos47197 <carlos_alfonso_e@hotmail.com>
2014-03-20 23:23:11 -0600

    unidad2

---
+++ MeHazVisto/Controllers/PetController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeHazVisto.Controllers
+{
+    public class PetController : Controller
+    {
+        //
+        // GET: /Pet/
+
+        public ActionResult Display()
+        {
+            
+            var name = (string)RouteData.Values["id"];
+         //   var model = PetManagement.GetByName(name);
+           // if (model == null)
+            return RedirectToAction("NotFound");
+            //return View(model);
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        
+        }
+
+        public FileResult DownLoadPicture()
+        {
+            var name = (string)RouteData.Values["id"];
+            var picture = "/Content/Uploads/" + name + ".jpg";
+            var contentType = "image/jpg";
+            var fileName = name + ".jpg";
+            return File(picture, contentType, fileName);
+        }
+
+    }
+}
